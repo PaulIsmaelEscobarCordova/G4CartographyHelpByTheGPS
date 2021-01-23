5198aaf2a57129444ed67650914fba62be66d1ff
KevinGuairacaja <mrkevin99@outlook.com>
2021-01-23 14:51:45 -0500

    Project 1 Group4 GPS

--- G4Cartography/src/g4cartography/G4Cartography.java
+++ G4Cartography/src/g4cartography/G4Cartography.java
@@ -156,6 +156,150 @@ public class G4Cartography {
                         
                     }                
                 }while(option != 2);
-    }
+            
+    
+            case 2:
+                do {
+                    System.out.println("Surface Area and Perimeter Calculator");
+                    System.out.println("Please Choose the Number ->  ");
+                    System.out.println("1. -> Perimeter");
+                    System.out.println("2. -> Area");
+                    System.out.println("0. -> Exit");
+                    option = input.nextInt();
+        
+                    switch(option){
+                        
+                        case 1:
+                            do {
+                                System.out.println("Perimeter Calculator");
+                                System.out.println("Please Choose the Number -> ");
+                                System.out.println("1. ==> 3 Points");
+                                System.out.println("2. ==> 4 Points");
+                                System.out.println("3. ==> 5 Points");
+                                System.out.println("0. ==> Exit");
+                                option = input.nextInt();
+                                        
+                                switch(option){
+            
+                                    case 1: 
+                                        double px1;
+                                        double px2;
+                                        double px3;
+                                        double py1;
+                                        double py2;
+                                        double py3;
+                                        double per;
+                                        System.out.println("Enter Px1 value -> ");
+                                        px1 = input.nextDouble();
+                                        System.out.println("Enter Py1 value -> ");
+                                        py1 = input.nextDouble();
+                                        System.out.println("Enter Px2 value -> ");
+                                        px2 = input.nextDouble();
+                                        System.out.println("Enter Py2 value -> ");
+                                        py2 = input.nextDouble();
+                                        System.out.println("Enter Px3 value -> ");
+                                        px3 = input.nextDouble();
+                                        System.out.println("Enter Py3 value -> ");
+                                        py3 = input.nextDouble();
+                                        per = Math.sqrt((px2 - px1)*(px2 - px1)+(py2 - py1)*(py2 - py1))
+                                                +Math.sqrt((px3 - px2)*(px3 - px2)+(py3 - py2)*(py3 - py2))
+                                                +Math.sqrt((px1 - px3)*(px1 - px3)+(py1 - py3)*(py1 - py3));
+                                        System.out.println("The Perimeter is: \n"
+                                        + per);
+                                        break;
+                                                
+                                    case 2: 
+                                        double px11;
+                                        double px22;
+                                        double px33;
+                                        double px44;
+                                        double py11;
+                                        double py22;
+                                        double py33;
+                                        double py44;
+                                        double per2;
+                                        
+                                        System.out.println("Enter Px1 value -> ");
+                                        px11 = input.nextDouble();
+                                        System.out.println("Enter Py1 value -> ");
+                                        py11 = input.nextDouble();
+                                        System.out.println("Enter Px2 value -> ");
+                                        px22 = input.nextDouble();
+                                        System.out.println("Enter Py2 value -> ");
+                                        py22 = input.nextDouble();
+                                        System.out.println("Enter Px3 value -> ");
+                                        px33 = input.nextDouble();
+                                        System.out.println("Enter Py3 value -> ");
+                                        py33 = input.nextDouble();
+                                        System.out.println("Enter Px4 value -> ");
+                                        px44 = input.nextDouble();
+                                        System.out.println("Enter Py4 value -> ");
+                                        py44 = input.nextDouble();
+                                        
+                                        per2 = Math.sqrt((px22 - px11)*(px22 - px11)+(py22 - py11)*(py22 - py11))
+                                                +Math.sqrt((px33 - px22)*(px33 - px22)+(py33 - py22)*(py33 - py22))
+                                                +Math.sqrt((px44 - px33)*(px44 - px33)+(py44 - py33)*(py44 - py33))
+                                                +Math.sqrt((px11 - px44)*(px11 - px44)+(py11 - py44)*(py11 - py44));
+                                        System.out.println("The Perimeter is: \n"
+                                        + per2);
+                                        break;
+                                    
+                                    case 3: 
+                                        double px111;
+                                        double px222;
+                                        double px333;
+                                        double px444;
+                                        double px555;
+                                        double py111;
+                                        double py222;
+                                        double py333;
+                                        double py444;
+                                        double py555;
+                                        double per3;
+                                        
+                                        System.out.println("Enter Px1 value -> ");
+                                        px111 = input.nextDouble();
+                                        System.out.println("Enter Py1 value -> ");
+                                        py111 = input.nextDouble();
+                                        System.out.println("Enter Px2 value -> ");
+                                        px222 = input.nextDouble();
+                                        System.out.println("Enter Py2 value -> ");
+                                        py222 = input.nextDouble();
+                                        System.out.println("Enter Px3 value -> ");
+                                        px333 = input.nextDouble();
+                                        System.out.println("Enter Py3 value -> ");
+                                        py333 = input.nextDouble();
+                                        System.out.println("Enter Px4 value -> ");
+                                        px444 = input.nextDouble();
+                                        System.out.println("Enter Py4 value -> ");
+                                        py444 = input.nextDouble();
+                                        System.out.println("Enter Px5 value -> ");
+                                        px555 = input.nextDouble();
+                                        System.out.println("Enter Py5 value -> ");
+                                        py555 = input.nextDouble();
+                                        
+                                        per3 = Math.sqrt((px222 - px111)*(px222 - px111)+(py222 - py111)*(py222 - py111))
+                                                +Math.sqrt((px333 - px222)*(px333 - px222)+(py333 - py222)*(py333 - py222))
+                                                +Math.sqrt((px444 - px333)*(px444 - px333)+(py444 - py333)*(py444 - py333))
+                                                +Math.sqrt((px555 - px444)*(px555 - px444)+(py555 - py444)*(py555 - py444))
+                                                +Math.sqrt((px111 - px555)*(px111 - px555)+(py111 - py555)*(py111 - py555));
+                                        System.out.println("The Perimeter is: \n"
+                                        + per3);
+                                        break;
+                                        
+                                                
+                                    case 0:
+                                        System.out.println("Good Luck \n");
+                                        System.exit(0);
+                                        break;
+                                    default:
+                                        System.out.println("Invalid option \n");
+                                        break;
+                                                        
+                                } 
+                            }while(option != 3);
+                                    
+
+        }
     
 }
